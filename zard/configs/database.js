6fdaa703d4e1e87a1886dc23752c7384f0ab8125
Zard.js <134866598+zardjs@users.noreply.github.com>
2023-05-28 23:24:15 -0300

    Disable logging

--- zard/configs/database.js
+++ zard/configs/database.js
@@ -1,16 +1,11 @@
 const Sequelize = require('sequelize');
 const readConfig = require('../common/utils');
-
-
-
-
 const { database, host, user, password, dialect } = readConfig().database;
-
-
 
 const sequelize = new Sequelize(database, user, password, {
   host: host,
-  dialect: dialect
+  dialect: dialect,
+  logging: false
 });
 
 module.exports = sequelize;
