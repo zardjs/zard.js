662cf7f9bae6fbdb2ae930fde5f6adca4b4d4c50
Zard.js <134866598+zardjs@users.noreply.github.com>
2023-05-30 21:20:33 -0300

    Update utils.js

--- zard/common/utils.js
+++ zard/common/utils.js
@@ -3,8 +3,8 @@ const root = GetResourcePath(GetCurrentResourceName());
 
 function readConfig() {
   try {
-    const configData = fs.readFileSync(`${root}/configs/config.json`, 'utf8');
-    return JSON.parse(configData);
+    const config = fs.readFileSync(`${root}/configs/config.json`, 'utf8');
+    return JSON.parse(config);
   } catch (error) {
     console.error('Erro ao ler o arquivo de configuração:', error);
     return null;
