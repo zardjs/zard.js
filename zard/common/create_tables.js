c3b65e4610bab8fe86b2a563f1a988c41bebc836
Zard.js <134866598+zardjs@users.noreply.github.com>
2023-05-30 21:48:40 -0300

    Update add Banishment

--- zard/common/create_tables.js
+++ zard/common/create_tables.js
@@ -1,16 +1,16 @@
-const { Player, PlayerData } = require('../models/players');
+const { Player, PlayerData, Banishment } = require('../models/players');
 
 async function checkAndCreateTables() {
   try {
-    // Verificar e criar a tabela "users"
     const PlayerTable = await Player.sync();
-    const PlayerData = await PlayerData.sync();
-    if (!PlayerTable && !PlayerData) {
+    const PlayerDataTable  = await PlayerData.sync();
+    const BanishmentDataTable  = await Banishment.sync();
+    if (!PlayerTable && !PlayerDataTable && !BanishmentDataTable) {
       await Player.sync({ force: true });
-      await PlayerData.sync({ force: true });
+      await PlayerDataTable.sync({ force: true });
+      await BanishmentDataTable.sync({ force: true });
       console.log('Tabela "player" criada com sucesso!');
     }
-
     console.log('Todas as tabelas foram verificadas e criadas, se necessário.');
   } catch (error) {
     console.error('Erro ao verificar e criar tabelas:', error);
