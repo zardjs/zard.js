dd685b9fbb7bedb422ff3f6c808c44e82e8857e5
Zard.js <134866598+zardjs@users.noreply.github.com>
2023-05-30 21:17:42 -0300

    Update zard.js

--- zard/zard.js
+++ zard/zard.js
@@ -1,5 +1,4 @@
-const fs = require('fs');
-const sequelize = require('./configs/database');
+const {sequelize, Op } = require('./configs/database');
 const  readConfig  = require('./common/utils');
 const { checkAndCreateTables } = require('./common/create_tables');
 
@@ -8,9 +7,10 @@ console.log("|___  /                 | |    (_)");
 console.log("   / /   __ _  _ __   __| |     _  ___");
 console.log("  / /   / _` || '__| / _` |    | |/ __|");
 console.log("./ /___| (_| || |   | (_| | _  | |\__ \\");
-console.log("\\_____/\\__,_||_|  \\__,_|(_) | ||___/");
+console.log("\\_____/ \\__,_||_|    \\__,_|(_) | ||___/");
 console.log("                              _/ |");
 console.log("                             |__/");
+console.log("==================== Framework Fivem ==========");
 
 class Zard {
   constructor() {
@@ -38,24 +38,19 @@ class Zard {
 
   async initializeDatabase() {
     try {
-      // Testa a conexão com o banco de dados
       await sequelize.authenticate();
       console.log('Conexão com o banco de dados estabelecida com sucesso!');
     } catch (error) {
-      console.error('Erro ao conectar ao banco de dados:', error);
+      console.error('Erro ao conectar ao banco de dados!');
       return;
     }
-
-
     try {
-      // Sincroniza o banco de dados
       await sequelize.sync();
       console.log('Banco de dados sincronizado com sucesso!');
       await checkAndCreateTables();
     } catch (error) {
       console.error('Erro ao sincronizar o banco de dados');
     }
-
     this.connection = sequelize;
   }
 }
