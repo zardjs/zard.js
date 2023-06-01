d2553c9d7d9c054f52c297fff6e9ac4d9372af27
Zard.js <134866598+zardjs@users.noreply.github.com>
2023-06-01 00:44:34 -0300

    Update zard.js

--- zard/zard.js
+++ zard/zard.js
@@ -1,59 +1,70 @@
-const {sequelize, Op } = require('./configs/database');
-const  readConfig  = require('./common/utils');
-const { checkAndCreateTables } = require('./common/create_tables');
+const { sequelize } = require('./configs/database')
+const readConfig = require('./common/config')
+const { Locales, loadLocale } = require('./utils/i18n')
+const { handleError, handleSuccess } = require('./utils/error')
+const { checkAndCreateTables } = require('./common/create_tables')
+const loadserver = require('./server/load')
 
-console.log(" ______                  _      _");
-console.log("|___  /                 | |    (_)");
-console.log("   / /   __ _  _ __   __| |     _  ___");
-console.log("  / /   / _` || '__| / _` |    | |/ __|");
-console.log("./ /___| (_| || |   | (_| | _  | |\__ \\");
-console.log("\\_____/ \\__,_||_|    \\__,_|(_) | ||___/");
-console.log("                              _/ |");
-console.log("                             |__/");
-console.log("==================== Framework Fivem ==========");
+console.log(' ______                  _      _')
+console.log('|___  /                 | |    (_)')
+console.log('   / /   __ _  _ __   __| |     _  ___')
+console.log("  / /   / _` || '__| / _` |    | |/ __|")
+console.log('./ /___| (_| || |   | (_| | _  | |__ \\')
+console.log('\\_____/ \\__,_||_|    \\__,_|(_) | ||___/')
+console.log('                              _/ |')
+console.log('                             |__/')
+console.log('==================== Framework Fivem ==========')
 
 class Zard {
+  static debug = false
+  static server = loadserver
   constructor() {
-    this.connection = null;
-    this.initialized = false;
-    this.initialize();
+    this.connection = null
+    this.initialized = false
+    this.initialize()
   }
 
   initialize() {
-    this.loadConfig();
+    this.loadConfig()
     if (!this.initialized) {
-      this.initializeDatabase();
-      this.initialized = true;
+      this.initializeDatabase()
+      this.initialized = true
     }
   }
 
   loadConfig() {
     try {
-      const config = readConfig();
-      this.language = config.language;
+      const config = readConfig()
+      Zard.debug = config.debug
+      this.language = config.language
+      loadLocale(config.language)
     } catch (err) {
-      console.error('Erro ao carregar as configurações:', err);
+      handleError(Locales[this.language]?.errorLoadingConfig, err)
     }
   }
 
   async initializeDatabase() {
+    this.connection = sequelize
     try {
-      await sequelize.authenticate();
-      console.log('Conexão com o banco de dados estabelecida com sucesso!');
+      await sequelize.authenticate()
+      handleSuccess(Locales[this.language]?.databaseConnectionSuccess)
     } catch (error) {
-      console.error('Erro ao conectar ao banco de dados!');
-      return;
+      handleError(Locales[this.language]?.errorInitializingDatabase, err)
+      return
     }
     try {
-      await sequelize.sync();
-      console.log('Banco de dados sincronizado com sucesso!');
-      await checkAndCreateTables();
+      await sequelize.sync()
+      await checkAndCreateTables()
+      handleSuccess(Locales[this.language]?.databaseSyncSuccess)
     } catch (error) {
-      console.error('Erro ao sincronizar o banco de dados');
+      handleError(Locales[this.language]?.errorSyncingDatabase, err)
     }
-    this.connection = sequelize;
+  }
+
+  static initialize() {
+    const zard = new Zard()
+    return zard
   }
 }
 
-const zardInstance = new Zard();
-zardInstance.initialize();
+Zard.initialize()
