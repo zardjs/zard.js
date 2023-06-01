a702742e8f589a4846de0fef0b714e360b817b34
Zard.js <134866598+zardjs@users.noreply.github.com>
2023-06-01 00:41:03 -0300

    Update database.js

--- zard/configs/database.js
+++ zard/configs/database.js
@@ -1,11 +1,11 @@
-const Sequelize = require('sequelize');
-const readConfig = require('../common/utils');
-const { database, host, user, password, dialect } = readConfig().database;
+const Sequelize = require('sequelize')
+const readConfig = require('../common/config')
+const { database, host, user, password, dialect } = readConfig().database
 
 const sequelize = new Sequelize(database, user, password, {
   host: host,
   dialect: dialect,
-  logging: false
-});
+  logging: false,
+})
 
-module.exports = { sequelize, Op: Sequelize.Op };
+module.exports = { sequelize, Op: Sequelize.Op }
