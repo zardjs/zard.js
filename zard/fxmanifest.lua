d048e4ae938ac9ad2044712ccb5f384385a6c8e3
Zard.js <134866598+zardjs@users.noreply.github.com>
2023-06-01 00:44:02 -0300

    Update fxmanifest.lua

--- zard/fxmanifest.lua
+++ zard/fxmanifest.lua
@@ -4,12 +4,14 @@ version '0.0.1'
 author 'Zard Framework'
 
 client_scripts {
+    'client/*.js',
     'zard.js'
 }
 
 server_scripts {
-    'zard.js',
-    'server/*'
+	'zard.js',
+    'server/events.js',
+    'utils/data.js'
 }
 
 files{
