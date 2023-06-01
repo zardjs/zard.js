02b85ddbfb62ab676650ae30df5da5dadb53f105
Zard.js <134866598+zardjs@users.noreply.github.com>
2023-06-01 00:39:54 -0300

    Update create_tables.js

--- zard/common/create_tables.js
+++ zard/common/create_tables.js
@@ -1,20 +1,21 @@
-const { Player, PlayerData, Banishment } = require('../models/players');
+const { Player, PlayerData, Banishment } = require('../models/players')
 
 async function checkAndCreateTables() {
   try {
-    const PlayerTable = await Player.sync();
-    const PlayerDataTable  = await PlayerData.sync();
-    const BanishmentDataTable  = await Banishment.sync();
+    const PlayerTable = await Player.sync()
+    const PlayerDataTable = await PlayerData.sync()
+    const BanishmentDataTable = await Banishment.sync()
     if (!PlayerTable && !PlayerDataTable && !BanishmentDataTable) {
-      await Player.sync({ force: true });
-      await PlayerDataTable.sync({ force: true });
-      await BanishmentDataTable.sync({ force: true });
-      console.log('Tabela "player" criada com sucesso!');
+      await Player.sync({ force: true })
+      await PlayerDataTable.sync({ force: true })
+      await BanishmentDataTable.sync({ force: true })
+      console.log('Tabela "player" criada com sucesso!')
     }
-    console.log('Todas as tabelas foram verificadas e criadas, se necessário.');
+
+    console.log('Todas as tabelas foram verificadas e criadas, se necessário.')
   } catch (error) {
-    console.error('Erro ao verificar e criar tabelas:', error);
+    console.error('Erro ao verificar e criar tabelas:', error)
   }
 }
 
-module.exports = { checkAndCreateTables };
+module.exports = { checkAndCreateTables }
